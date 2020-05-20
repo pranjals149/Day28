51b5be485b31f3f2370dc538f52c847cf40aae3d
Pranjal Srivastava <pranjals149@gmail.com>
2020-05-20 16:55:10 +0530

    Day28

---
+++ routes/users.js
@@ -0,0 +1,35 @@
+var express = require('express');
+const bodyParser = require('body-parser');
+var User = require('../models/user')
+var passport = require('passport');
+
+var router = express.Router();
+router.use(bodyParser.json())
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.post('/signup', (req, res, next) => {
+  User.register(new User({ username: req.body.username }), req.body.password), (user, err) => {
+    if(err) {
+      res.statusCode = 500;
+      res.setHeader('Content-Type', 'application/json')
+      res.json({err: err})
+    } else {
+      passport.authenticate('local')(req, res, () => {
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json')
+        res.json({ success: true, status: 'Registration Successfull'})
+      })
+    } 
+  }
+})
+
+router.post('/login', passport.authenticate('local'), (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json')
+  res.json({ success: true, status: 'You are successfully logged in'})
+})
+
+module.exports = router;
